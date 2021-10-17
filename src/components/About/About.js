7a0b178e1a7733a599eb08d72213e89d6956e7ce
isti2623 <istiaqueahmed2019.bd@gmail.com>
2021-10-18 05:48:02 +0600

    add contact about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import './About.css'
 
 const About = () => {
     return (
         <div>
-            <h2>about</h2>
+            <div className='about-img'>
+
+
+
+
+            </div>
+            <div className='container'>
+                <h2 className='my-5 text-center fw-bold'>We make digital products and help organistation big and small  with their audience.</h2>
+                <p>It was a big, round room with a high arched roof, and the walls and ceiling and floor were covered with large emeralds set closely together.</p>
+
+                <p>In a word, the whale was seized and sold, and his Grace the Duke of Wellington received the money. Thinking that viewed in some particular lights, the case might by a bare possibility in some small degree be deemed, under the circumstances, a rather hard one, an honest clergyman of the town addressed a note to his Grace.</p>
+
+                <h2>OUR CENTER
+                </h2>
+
+                <p>It was shaped like a chair and sparkled with gems, as did everything else. In the center of the chair was an enormous Head, without a body to support it or any arms or legs whatever.</p>
+                <div className="row my-5">
+                    <div className="col-md-6">
+                        <h2>MELBOURNE</h2>
+                        <p>258 New Design St, VIC 10000, Austria <br />
+                            +0900 8618 3725 69 <br />
+                            hello@hope.com</p>
+                    </div>
+
+                    <div className="col-md-6">
+                        <h2>NEWYORK</h2>
+                        <p>3rd Floor, 692 Orchard St, NY 10000, Newyork<br />
+                            +04200 4739 6899 6880 <br />
+                            office@hope.com
+                        </p>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 };
