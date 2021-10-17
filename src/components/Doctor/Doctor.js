cffa3056fd11e387cc4af58d2c1c55e5c27222f1
isti2623 <istiaqueahmed2019.bd@gmail.com>
2021-10-18 04:09:17 +0600

    update doctor

--- src/components/Doctor/Doctor.js
+++ src/components/Doctor/Doctor.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Card, Col, Row } from 'react-bootstrap';
+
 
 const Doctor = () => {
+    const [doctor, setDoctor] = useState([]);
+    useEffect(() => {
+        fetch('/doctor.json')
+            .then(res => res.json())
+            .then(data => setDoctor(data))
+    }, [])
     return (
         <div>
-            <h2>Doctor</h2>
+            <h2 className='text-center text-danger fs-1 fw-bold mt-3'>Our Specialist</h2>
+            <div className='container'>
+                <div className='my-5'>
+                    <Row xs={1} md={2} className="g-4">
+                        {
+                            doctor.map(docs => <Col>
+                                <Card >
+                                    <Card.Img height='350px' variant="top" src={docs.image} />
+                                    <Card.Body>
+                                        <Card.Title>{docs.title}</Card.Title>
+                                    </Card.Body>
+                                </Card>
+                            </Col>)
+                        }
+                    </Row>
+                </div>
+            </div>
+            );
+
+
         </div>
     );
 };
