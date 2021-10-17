0068112de3e01f37eadfa5e175b46dd082ec6d43
isti2623 <istiaqueahmed2019.bd@gmail.com>
2021-10-18 04:12:57 +0600

    update

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,6 +1,7 @@
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './Banner.css'
 
@@ -13,7 +14,7 @@ const Banner = () => {
                         <div>
                             <h6 className='text-white'>FEEL THE DIFFERENCE WITH US</h6>
                             <h2 className='text-part'>Your Feels Is Our <br /> <span className='text-danger'>Priority</span> </h2>
-                            <div className="btn btn-danger fw-bold">OUR DOCTORS <FontAwesomeIcon className='text-white mt-2' icon={faArrowRight} /></div>
+                            <Link to='/doctor'><div className="btn btn-danger fw-bold">OUR DOCTORS <FontAwesomeIcon className='text-white mt-2' icon={faArrowRight} /></div></Link>
                         </div>
                     </div>
                 </div>
