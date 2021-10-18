1e3fecb2b76df13ac9a85ed6a5dfcb8c5fc7b61e
isti2623 <istiaqueahmed2019.bd@gmail.com>
2021-10-18 13:12:35 +0600

    update

--- src/components/Doctor/Doctor.js
+++ src/components/Doctor/Doctor.js
@@ -13,19 +13,20 @@ const Doctor = () => {
         <div>
             <h2 className='text-center text-danger fs-1 fw-bold mt-3'>Our Specialist</h2>
             <div className='container'>
+
                 <div className='my-5'>
-                    <Row xs={1} md={2} className="g-4">
+
+                    <div className='row'>
+
                         {
-                            doctor.map(docs => <Col>
-                                <Card >
-                                    <Card.Img height='350px' variant="top" src={docs.image} />
-                                    <Card.Body>
-                                        <Card.Title>{docs.title}</Card.Title>
-                                    </Card.Body>
-                                </Card>
-                            </Col>)
+                            doctor.map(docs => <div className="col-lg-6">
+                                <img height='450px' className='w-100' src={docs.image} alt="" srcset="" />
+                                <h2 className='text-center text-danger my-3 fw-bold'>{docs.title}</h2>
+                            </div>)
                         }
-                    </Row>
+
+                    </div>
+
                 </div>
             </div>
 
