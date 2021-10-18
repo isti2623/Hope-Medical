3cd3ce9cbdcf61ec01335de6dd8dacab238dc7a6
isti2623 <istiaqueahmed2019.bd@gmail.com>
2021-10-18 13:45:37 +0600

    final update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,24 +5,26 @@ import logo from '../../images/logo.png'
 const Footer = () => {
     return (
         <div>
-            <div className='footer-part mt-5'>
-                <div className="row">
-                    <div className="col-lg-6 col-md-12 ms-5 mt-5">
-                        <img className='w-25 ms-5 mt-3 foot-image' src={logo} alt="" />
-                        <p className='container text-white mb-5 ms-5 mt-5'>copyright 2022 Hope Medical</p>
-                    </div>
-                    <div className="col-lg-6 col-md-12">
-                        <ul className='text-decoration-none text-white my-5'>
-                            <br />
-                            <br />
-                            <br />
-                            <li>About Hope Medical</li>
-                            <li>Read Our Blog</li>
-                            <li>Signup to Hope</li>
-                            <li>Add your Hospital</li>
-                        </ul>
-                    </div>
+            <div className='footer-part mt-5 '>
+                <div className='container'>
+                    <div className="row">
+                        <div className="col-lg-4 col-md-4 ms-5 mt-5">
+                            <img className='w-25 ms-5 mt-3 foot-image' src={logo} alt="" />
+                            <p className='container text-white mb-5 ms-5 mt-5'>copyright 2022 Hope Medical</p>
+                        </div>
+                        <div className="col-lg-4 col-md-4">
+                            <ul className='text-decoration-none text-white my-5 ms-5'>
+                                <br />
+                                <br />
+                                <br />
+                                <li>About Hope Medical</li>
+                                <li>Read Our Blog</li>
+                                <li>Signup to Hope</li>
+                                <li>Add your Hospital</li>
+                            </ul>
+                        </div>
 
+                    </div>
                 </div>
             </div>
         </div>
